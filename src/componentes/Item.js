2487014305dfd6572c9d202ef1d7feecdaef56cf
Maxi <myazlle99@gmail.com>
2022-10-03 21:10:50 -0300

    Implemento componente Item

--- src/componentes/Item.js
+++ src/componentes/Item.js
@@ -2,11 +2,11 @@
 // 1- crear un <li> que tenga su click preparado para ejecutar el metodo heredado para seleccionar la plataforma como favorita
 // 2- Presentar el nombre de la plataforma que le llega como prop
 
-export default function Item(props) {
+export default function Item({ nombre, handleFavorita }) {
     return (
-      <>
-          // 🚩
-      </>
+      <li onClick={ () => { handleFavorita(nombre)} }>
+        {nombre}
+      </li>
     );
   }
   
